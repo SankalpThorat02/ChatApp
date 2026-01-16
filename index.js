9541e3eb07d0db500536da80b1205222c11fcf85
SankalpThorat02 <sankalpt2225@gmail.com>
2026-01-16 23:14:51 +0530

    Chat Model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 const port = 8080;
 
@@ -15,6 +16,16 @@ main().then(() => console.log("Connection Successful"))
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+
+const chat = new Chat({
+    from: "Sankalp",
+    to: "Dny",
+    msg: "Hello",
+    created_at: new Date()
+});
+
+chat.save().then(res => console.log(res))
+    .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`App is listening at ${port}`);
