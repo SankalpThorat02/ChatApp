51f4bc8a98919c5e1f9c710fbebbed6dff15870e
SankalpThorat02 <sankalpt2225@gmail.com>
2026-01-16 22:21:34 +0530

    Basic Setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const path = require("path");
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+main().then(() => console.log("Connection Successful"))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+app.listen(port, () => {
+    console.log(`App is listening at ${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+})
